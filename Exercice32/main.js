d2149e4176918a2c7e4752de0b4d76ead045303d
Fatima BENAJJOU <fatima.benajjou@gmail.com>
2024-08-28 16:41:29 +0200

    exercice PokéAPIé

---
+++ Exercice32/main.js
@@ -0,0 +1,46 @@
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+const output = document.getElementById("output");
+const searchPokemon= document.getElementById("pokemonInput")
+
+// Fonction pour afficher les résultats dans le bloc <pre>
+const displayResult = (data) => {
+    output.textContent = JSON.stringify(data, null, 2);
+};
+
+
+
+const getPosts = async () => {
+    try {
+        const response = await fetch(apiUrl); // Requête GET pour récupérer tous les posts
+        const data = await response.json();  // Conversion de la réponse en format JSON
+        displayResult(data); // Affichage de tous les posts
+        console.log(data[39])
+    } catch (error) {
+        displayResult({ error: "Erreur lors de la récupération des posts" }); // Gestion des erreurs
+    }
+};
+
+// Fonction pour récupérer un post spécifique par ID
+const getPostById = async () => {
+    const searchPokemon= document.getElementById("pokemonInput")
+
+    if (searchPokemon) {
+        try {
+            const response = await fetch(`${apiUrl}/${searchPokemon}`); // Requête GET avec l'ID du post à récupérer
+            const data = await response.json(); // Conversion de la réponse en format JSON
+            displayResult(data); // Affichage du post spécifié
+           
+        } catch (error) {
+            displayResult({ error: `Erreur lors de la récupération du pokémon ${searchPokemon}}` }); // Gestion des erreurs
+        }
+    }
+};
+
+
+
+
+
+// Attachement des fonctions aux boutons
+document.getElementById("getAllBtn").addEventListener("click", getPosts);
+document.getElementById("getByIdBtn").addEventListener("click", getPostById);
+
